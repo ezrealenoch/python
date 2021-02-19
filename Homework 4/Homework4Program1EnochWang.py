ca904e8d0275289b106a87279800e9aa4c549cf0
enochnekowang <58448136+enochnekowang@users.noreply.github.com>
2021-02-19 12:40:31 -0500

    Homework 4 Part 2 Done

---
+++ Homework 4/Homework4Program1EnochWang.py
@@ -0,0 +1,14 @@
+# Enoch Wang
+# 2/19/2020
+# Homework 4 Program 1
+# CSCI 6651
+# Professor Gulnora Nurmatova 
+
+def celsius(F):
+    Tc = (5/9)*(F-32)
+    return Tc
+    
+    
+if __name__ == '__main__': 
+    print(celsius(68))
+    
---
+++ Homework 4/Homework4Program2EnochWang.py
@@ -0,0 +1,43 @@
+# Enoch Wang
+# 2/19/2020
+# Homework 4 Program 2
+# CSCI 6651
+# Professor Gulnora Nurmatova 
+
+import sys
+
+def count_frequency(mylist):
+    newlist = []
+    iterable = []
+    uniqueList = list(set(mylist))
+    uniques = len(uniqueList)
+    for i in range(0,uniques):
+        count = 0
+        for j in range(0,len(mylist)):
+            if uniqueList[i] == mylist[j]:
+                count = count + 1
+        newlist.append(count)
+        
+    # Sorting by Frequency Work in Progress
+    
+    # if len(uniqueList) == len(newlist):
+    #     min = sys.maxsize
+    #     max = 0 
+    #     for i in range(0,len(newlist)):
+    #         pos = 0
+    #         for j in range(0,len(newlist)):
+    #             if min > newlist[j] and max < newlist[j]:
+    #                 min = newlist[j]
+    #                 pos = j
+    #         iterable.append(pos)
+    #         max = min
+    
+    dic = {uniqueList[i]: newlist[i] for i in range(len(uniqueList))}
+    return dic
+    
+    
+if __name__ == '__main__': 
+    mylist=["one", "two","eleven",  "one", "three", "two", "eleven", "three", "seven", "eleven"]
+    print(count_frequency(mylist))
+    
+    
